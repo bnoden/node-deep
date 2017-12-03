3c0356bcac58e94bbbf2a73a8c2935f8867b599b
bnoden <brandroid369@gmail.com>
2017-12-03 08:15:16 -0600

    show if pass or fail

--- _test/decToHex.js
+++ _test/decToHex.js
@@ -2,18 +2,37 @@ const fs = require('fs');
 const { decToHex } = require('../decToHex');
 
 const decArray = ['', 'abc', 1224, 13.5, 255, 256, 65535, -65535, 3, -3];
+const solution = [
+  'decToHex(): Input must be a number',
+  'decToHex(): Input must be a number',
+  '4c8',
+  'd',
+  'ff',
+  '100',
+  'ffff',
+  'ffff0001',
+  '3',
+  'fffffffd'
+];
 
-const samples = (s, o) => {
+const samples = (s, o, numberPassed, pass) => {
   s = '';
-  o = '';
+  numberPassed = 0;
   for (let i in decArray) {
+    if (solution[i] === decToHex(decArray[i])) {
+      numberPassed++;
+      pass = 'PASS';
+    } else {
+      pass = 'FAIL';
+    }
     o = decToHex(decArray[i]).split(' ').length > 1 ? '' : '0x';
-    s += `${decArray[i]} = ${o}${decToHex(decArray[i])}\n`;
+    s += `${decArray[i]} = ${o}${decToHex(decArray[i])} ${pass}\n`;
   }
+  s += `\n${numberPassed} of ${decArray.length} tests passed\n`;
   return s;
 };
 
-const message = 'refactor';
+const message = 'Show if pass or fail';
 
 const testSample = () => {
   return `\n${Date()}\nmessage: ${message}\n\nBEGIN\n${samples()}END\n`;
