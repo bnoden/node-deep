9553d28f84be1ebf21967e75f927effc1f38f66d
bnoden <brandroid369@gmail.com>
2017-12-24 08:20:58 -0600

    mk _redux_react_2

---
+++ _redux_react_2/src/index.js
@@ -0,0 +1,40 @@
+import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import appReducer from './reducers'
+import { createUser, createPost } from './actions'
+import App from './App'
+
+let store = createStore(appReducer)
+
+store.dispatch(createUser('bnoden', 'Brandon Oden'))
+store.dispatch(createUser('midimaster2000', 'Yoko Shimimura'))
+store.dispatch(createUser('golbez4u', 'Nobuo Uematsu'))
+
+store.dispatch(createPost('bnoden', {
+  title: 'first',
+  text: 'inb4 all',
+  category: 'welcome',
+}))
+store.dispatch(createPost('midimaster2000', {
+  title: 'first?',
+  text: "ninja'd",
+  category: 'test',
+}))
+store.dispatch(createPost('golbez4u', {
+  title: 'Haha',
+  text: 'lol u2',
+  category: 'test',
+}))
+
+console.log('initial state:', store.getState())
+store.subscribe(() =>
+  console.log('state changed:', store.getState())
+)
+
+ReactDOM.render(
+  <App store={store} />,
+  document.getElementById('root')
+)
+
