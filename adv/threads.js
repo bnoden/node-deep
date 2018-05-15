a46041359804e72d29933d933a640e1e937c3dc9
bnoden <brandroid369@gmail.com>
2018-05-15 02:35:02 -0500

    .

--- adv/threads.js
+++ adv/threads.js
@@ -2,26 +2,35 @@ const pbkdf2 = require('crypto').pbkdf2;
 const fs = require('fs');
 
 const text = {
-  log: str =>
-    fs.appendFile('log/threads_log.txt', str, err => err && str.append('error'))
+  log: str => {
+    console.log(str);
+    fs.appendFile(
+      'log/threads_log.txt',
+      `${str}\n`,
+      err => err && str.append('error')
+    );
+  }
 };
 
 const start = Date.now();
 
 const benchmark = (thread = 0) => {
-  pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    const log = `${thread}: ${0.001 * (Date.now() - start)} seconds`;
-    console.log(log);
-    text.log(`${log}\n`);
-  });
+  pbkdf2('a', 'b', 100000, 512, 'sha512', () =>
+    text.log(`${thread}: ${0.001 * (Date.now() - start)} seconds`)
+  );
 };
 
 const log_benchmarks = (i = 1, max = 12) => {
+  text.log(
+    `-----------------\nprocess.env.UV_THREADPOOL_SIZE = ${
+      process.env.UV_THREADPOOL_SIZE
+    }`
+  );
+
   while (i <= max) {
     benchmark(i);
     ++i;
   }
-  text.log('-----------------------\n');
 };
 
 log_benchmarks();
