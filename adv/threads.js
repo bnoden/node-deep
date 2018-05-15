8aa9c0538157f671bc0c3d2b96aee9c57ada59ab
bnoden <brandroid369@gmail.com>
2018-05-14 20:39:13 -0500

    .

--- adv/threads.js
+++ adv/threads.js
@@ -1,9 +1,9 @@
-const crypto = require('crypto');
+const pbkdf2 = require('crypto').pbkdf2;
 
 const start = Date.now();
 
 const benchmark = (str = '') =>
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () =>
+  pbkdf2('a', 'b', 100000, 512, 'sha512', () =>
     console.log(`${str}:`, `${0.001 * (Date.now() - start)} seconds`)
   );
 
